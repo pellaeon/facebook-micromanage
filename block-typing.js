b9df8b851f35e76936920f2c0a6cb03d5c4cf0ee
Pellaeon Lin <nfsmwlin@gmail.com>
2016-08-28 02:15:06 +0800

    Import, working, not tested from other's view

---
+++ block-typing.js
@@ -0,0 +1,18 @@
+chrome.webRequest.onBeforeRequest.addListener(
+		function(info) {
+			console.log("Cat intercepted: " + info.url);
+			return { cancel: true };
+		},
+		// filters
+		{
+			urls: [
+				"https://www.facebook.com/ajax/typeahead/*",
+				"https://www.facebook.com/ufi/typing/*",
+				"https://pixel.facebook.com/ajax/chat/opentab_tracking.php*",
+				"https://www.facebook.com/ajax/messaging/typ.php*"
+			],
+			types: ["xmlhttprequest"]
+		},
+		// extraInfoSpec
+		["blocking"]
+);
