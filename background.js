022a084e286580f6bd06d0bafd3250f00e8a02af
Pellaeon Lin <nfsmwlin@gmail.com>
2016-10-24 23:51:39 +0800

    Add basic friend list crawler

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+var COOKIES = [];
+var UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36";
+var me_id = '';
+
+// http://stackoverflow.com/questions/10334909/associate-a-custom-user-agent-to-a-specific-google-chrome-page-tab/10339902#10339902
 chrome.webRequest.onBeforeRequest.addListener(
 		function(info) {
 			console.log("Cat intercepted: " + info.url);
@@ -31,6 +36,76 @@ chrome.runtime.onMessage.addListener(
 			console.log(sender.tab ?
 				"from a content script:" + sender.tab.url :
 				"from the extension");
-			if (request.greeting == "hello")
-	sendResponse({farewell: "goodbye"});
+			if (request.type == "addItem") {
+				getFriendList();
+				sendResponse({farewell: "goodbye"});
+			}
 		});
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(info) {
+        // Replace the User-Agent header
+        var headers = info.requestHeaders;
+        headers.forEach(function(header, i) {
+            if (header.name.toLowerCase() == 'user-agent') { 
+                header.value = UA;
+            }
+        });  
+        return {requestHeaders: headers};
+    },
+    // Request filter
+    {
+        // Modify the headers for these pages
+        urls: [
+            "https://www.facebook.com/*"
+        ],
+        // In the main window and frames
+        types: ["main_frame", "sub_frame"]
+    },
+    ["blocking", "requestHeaders"]
+);
+
+function getMeId() {
+	r1 = new XMLHttpRequest();
+	r1.onreadystatechange = function() {
+		re = /USER_ID\":\"(\d+)\"/;
+		res = re.exec(this.responseText);
+		me_id = res[1];
+	};
+	r1.open('GET', 'https://www.facebook.com/me', false);
+	r1.send();
+}
+
+function getUserWall(id) {
+}
+
+function builddataQS(me_id, cursor='') {
+	data = {
+		'collection_token': me_id+':2356318349:2',//magic number
+		'cursor': btoa('0:not_structured:'+cursor),
+		"tab_key":"friends",
+		'profile_id': parseInt(me_id),//parseInt is significant
+		//"q":"",//not required
+		"overview":false,
+		"ftid":null,
+		"order":null,
+		"sk":"friends",
+		"importer_state":null
+	};
+	return encodeURIComponent(JSON.stringify(data));
+}
+function getFriendList(cursor) {
+	getMeId();
+	var site = 'https://www.facebook.com';
+	var endpoint = '/ajax/pagelet/generic.php/AllFriendsAppCollectionPagelet';
+	console.log(builddataQS(me_id));
+	var qs = '?dpr=1&data='+builddataQS(me_id, cursor)+'&__user='+me_id+'&__a=1&__dyn=';//__a=1 is significant
+	//getCookies();
+	r1 = new XMLHttpRequest();
+	r1.onreadystatechange = function() {
+	};
+	r1.open('GET', site+endpoint+qs, true);
+	//r1.setRequestHeader('user-agent', UA);
+	r1.send();
+
+}
--- wall.js
+++ wall.js
@@ -1,9 +1,18 @@
 console.log('hey wall');
 
 $('button#startcrawl').click(function() {
-	chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+	chrome.runtime.sendMessage({type: "addItem"}, function(response) {
 		  console.log(response);
 	});
 	$(this).html('Crawling...');
 	$(this).attr("disabled", true);
 });
+
+chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			console.log(sender.tab ?
+				"from a content script:" + sender.tab.url :
+				"from the extension");
+			if (request.type == "appendFriendList") {
+	sendResponse({farewell: "goodbye"});
+		});
